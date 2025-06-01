538b556698bd8ac44e9e752bf687bbdd7d98ffce
Abdelrahman <abdelrahman.ashraf.engineer@gmail.com>
2025-06-02 00:11:36 +0300

    merge with moderations app

--- Sadaqa/Sadaqa/settings.py
+++ Sadaqa/Sadaqa/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "projects.apps.ProjectsConfig",
     "engagement.apps.EngagementConfig",
-
-    #=========================================
-    
+    "moderations.apps.ModerationsConfig",
+    # =========================================
     "crispy_forms",
     "crispy_bootstrap5",
     # Django core apps
@@ -55,6 +54,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "rest_framework_simplejwt",
     "phonenumber_field",  # Adds phone number validation and formatting.
     "django_countries",  # Simplifies country dropdowns and data storage (e.g., "EG" for Egypt).
     # need to install pip install django-allauth to use the apps below
